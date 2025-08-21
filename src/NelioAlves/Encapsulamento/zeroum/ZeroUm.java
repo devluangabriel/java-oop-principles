3573b4289ace66d9f998d3ffc68b249d8724973b
Luan Gabriel <zuemeromualdo44@gmail.com>
2025-08-20 22:31:56 -0300

    Encapsulamento, Palavra this, Modificadores de acesso, Construtores

---
+++ src/NelioAlves/Encapsulamento/zeroum/ZeroUm.java
@@ -0,0 +1,41 @@
+package NelioAlves.Encapsulamento.zeroum;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ZeroUm {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Price: ");
+        Double price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        Integer quantityA = sc.nextInt();
+
+        NelioAlves.Construtores.ConstructorAndPalavraThisAndEncapsulamento.Aula01.Entity.ZeroUm zeroUm = new NelioAlves.Construtores.ConstructorAndPalavraThisAndEncapsulamento.Aula01.Entity.ZeroUm(name, price, quantityA);
+
+        System.out.println();
+        System.out.println("Product data: " );
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        zeroUm.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " );
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        zeroUm.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " );
+        sc.close();
+    }
+}
